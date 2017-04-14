984b2b2439c1f10dab4b4577565529ccec04bce9
Piotr <piotr.stachow@gmail.com>
2017-04-14 15:00:57 +0200

    Test - kurs, c.d. i zmiana nazw aktywności Tutorial niezakończona. Poprawnie działa włącznie z przyciskiem Szukaj w menu górnym

--- app/src/main/java/com/ps/bookweb/KonfigITPSActivity.java
+++ app/src/main/java/com/ps/bookweb/KonfigITPSActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by ps on 2017-03-06.
@@ -26,6 +31,9 @@ public class KonfigITPSActivity extends ActionBarActivity {
     //wywołana po wciśnięciu przycisku (do stworzenia eventu) z activity_tutorial.xml
     public void OnNextClick(View view) {
 
+        //Adapter element, który łączy ListView z danymi mającymi być prezentowane jako lista
+        private ArrayAdapter<String> adapter;
+
         /*
         intent - czyli zamiar uruchomienia nowej aktywności, tworzymy nowy obiektu klasy Intent
         zainicjowanie obiektu (new) poprzez wywołanie konstruktora Intent
@@ -33,7 +41,21 @@ public class KonfigITPSActivity extends ActionBarActivity {
         i uruchamiamy nową aktywność - nazawa aktywności - AboutActivity, parametrem jest intent1
         */
 
-        Intent intent1 = new Intent(this, HomeActivity.class);
+        //przechwytujemy referencję do widoku listowego
+        //i tworzymy obiekt listy tablicowej i przypisujemy mu naszą tablicę
+        ListView listViewLinki = (ListView) findViewById(R.id.listView1_linki);
+
+        String linki[] = {
+                getString(R.string.link1),
+                getString(R.string.link2),
+                getString(R.string.link3)};
+
+        ArrayList<String> linkiArray = new ArrayList<String>();
+        linkiArray.addAll( Arrays.asList(linki));
+        adapter = new ArrayAdapter<String>(this, R.layout.row, linkiArray);
+        listViewLinki.setAdapter(adapter);
+
+        Intent intent1 = new Intent(this, NetworkConfigActivity.class);
         startActivity(intent1);
 
     }
