d937e207d60f9f87c6b4eac8bf2906d46590213a
Piotr <piotr.stachow@gmail.com>
2017-04-20 15:19:14 +0200

    Test - kurs, nauka c.d. i zmiana widoku lista - dane w strings.xml - array.

--- app/src/main/java/com/ps/bookweb/MainITPSActivity.java
+++ app/src/main/java/com/ps/bookweb/MainITPSActivity.java
@@ -18,34 +18,27 @@ import java.util.Arrays;
  */
 
 public class MainITPSActivity extends ActionBarActivity {
+    private String[] linki;
+    private ListView listView_linki;
+    private ArrayAdapter adapter;
 
-    /*
-    : wskazanie aktywności żeby korzystała z odpowiedniego layotu, trzeba nadpisać metodę onCreate
-    : nadpisanie metody, wywołanie metody rodzica
-    : trzeba ustawić widok naszej aktywności i podać nazwę layoutu
-    */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainitps);
-
         /*
-        : Adapter element, który łączy ListView z danymi mającymi być prezentowane jako lista
-        : intent - czyli zamiar uruchomienia nowej aktywności, tworzymy nowy obiektu klasy Intent
-        : zainicjowanie obiektu (new) poprzez wywołanie konstruktora Intent
-        : this (czyli nasza klasa) - oznacza z jakiego kontekstu będzie uruchamiana nasza nowa aktywność
-        : i uruchamiamy nową aktywność - nazwa aktywności - AboutActivity, parametrem jest intent1
-
-        : przechwytujemy referencję do widoku listowego
-        : i tworzymy obiekt listy tablicowej i przypisujemy mu naszą tablicę
+        : wskazanie aktywności żeby korzystała z odpowiedniego layotu, trzeba nadpisać metodę onCreate
+        : nadpisanie metody, wywołanie metody rodzica
+        : trzeba ustawić widok naszej aktywności i podać nazwę layoutu
         */
 
-        ArrayAdapter adapter;
-        ListView listViewLinki = (ListView) findViewById(R.id.listView1_linki);
+        //przechwytujemy referencję do widoku listowego
+        listView_linki = (ListView) findViewById(R.id.listView_linki);
+        initPobierzLinki();
+        initListViewLinki();
 
-        pobierzLinki();
-        initLinkiListView();
         /*
+        inny sposób
         String linki[] = {
                 getString(R.string.link1),
                 getString(R.string.link2),
@@ -58,43 +51,57 @@ public class MainITPSActivity extends ActionBarActivity {
 
         ArrayList linkiArray = new ArrayList();
         linkiArray.addAll(Arrays.asList(linki));
+        adapter = new ArrayAdapter(this, R.layout.element_listview, linkiArray);
+        listViewLinki.setAdapter(adapter);
         */
-        //adapter = new ArrayAdapter(this, R.layout.element_listview, linkiArray);
-        //listViewLinki.setAdapter(adapter);
     }
 
-    private void pobierzLinki(){
+    public void initPobierzLinki(){
         /*
         : POBIERANIE DANYCH z /res/values/strings.xml
         : odbywa się to poprzez obiekt Resources, który wewnątrz klasy Aktywności można pobrać
         : za pomocą getResources(). Metoda ta zawraca obiekt dający nam dostęp do wszystkich zasobów
         : folderu /res/ naszej aplikacji.
 
-        : elementy zasobów naszej aplikacji pobieramy za pomocą metod get…(int). Zwracają one element,
+        : Elementy zasobów naszej aplikacji pobieramy za pomocą metod get…(int). Zwracają one element,
         : na którego ID wskazują pola statyczne automatycznie generowanej klasy R. My korzystamy
         : z metody getStringArray(int)
         */
 
         Resources dane = getResources();
-        String[] linki = dane.getStringArray(R.array.lista_linkow);
+        linki = dane.getStringArray(R.array.lista_linkow);
     }
 
 
-    private void initLinkiListView() {
-
-        listViewLinki.setAdapter(new ArrayAdapter<String>(
-                getApplicationContext(),
-                R.layout.element_listview,
-                linki));
+    private void initListViewLinki(){
+        /*
+        : INICJALIZACJA LISTVIEW
+        : Adapter element, który łączy ListView z danymi mającymi być prezentowane jako lista
+        : wzorzec Adapter, powinien dziedziczyć z klasy BaseAdapter. Android domyślnie posiada dwie
+        : implementacje adapterów – CursorAdapter oraz ArrayAdapter - który służy do „adaptowania” danych będących tablicami
+        : (CursorAdapter służy do obsługi danych bazodanowych
+        : Żeby element ListView wyświetlił przekazane mu dane musimy skorzystać z wzorca Adapter,
+        : Konstruktorowi, oprócz kontekstu aplikacji przekazujemy:
+        : - layout pojedynczego elementu listy (android.R.layout jest polem,
+        : w którym przechowywane są domyślne layouty, dostarczone wraz z systemem Android),
+        : - tablicę napisów, którą wcześniej pobraliśmy z zasobów naszej aplikacji.
+        */
+        adapter = new ArrayAdapter(getApplicationContext(), R.layout.element_listview, linki);
+        listView_linki.setAdapter(adapter);
     }
 
 
     //wygenerowane przez ALT+enter na nazwie metody która ma być
     //wywołana po wciśnięciu przycisku (do stworzenia eventu) z activity_tutorial.xml
     public void OnNextClick(View view) {
+        /*
+        : intent - czyli zamiar uruchomienia nowej aktywności, tworzymy nowy obiektu klasy Intent
+        : zainicjowanie obiektu (new) poprzez wywołanie konstruktora Intent
+        : this (czyli nasza klasa) - oznacza z jakiego kontekstu będzie uruchamiana nasza nowa aktywność
+        : i uruchamiamy nową aktywność - nazwa aktywności
+        */
 
         Intent intent1 = new Intent(this, NetworkConfigActivity.class);
         startActivity(intent1);
-
     }
 }
